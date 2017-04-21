1f918766e778336c77d36d2f41734a7f7feb6897
Erik Joling <erik@ejoweb.nl>
2017-04-21 11:25:35 +0200

    Enabled extra debug-settings in wp-config

--- provision/vvv-init.sh
+++ provision/vvv-init.sh
@@ -29,7 +29,15 @@ fi
 if [[ ! -f "${VVV_PATH_TO_SITE}/public_html/wp-config.php" ]]; then
   echo "Configuring WordPress Stable..."
   noroot wp core config --dbname="${DB_NAME}" --dbuser=wp --dbpass=wp --quiet --extra-php <<PHP
+/** For developers: WordPress debugging mode. */
 define( 'WP_DEBUG', true );
+define('SAVEQUERIES', false); // Only activate when researching
+
+/** Extra debugging */
+if (WP_DEBUG) {
+  define('SCRIPT_DEBUG', true);
+  define('WP_DEBUG_LOG', true);
+}
 PHP
 fi
 
